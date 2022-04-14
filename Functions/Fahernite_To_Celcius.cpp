cc4806c563989f832be453bc4282d26c2410f2c3
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-04-14 13:45:18 +0530

    completed

---
+++ Functions/Fahernite_To_Celcius.cpp
@@ -0,0 +1,9 @@
+void printTable(int start, int end, int step) 
+{
+     for(int i=start;i<=end;i+=step)
+     {
+        int  cel=(i-32)*5/9;
+         cout<<i<<"\t"<<cel<<endl;
+     }
+ }
+ 
---
+++ Functions/Fibonacci_Number.cpp
@@ -0,0 +1,14 @@
+bool checkMember(int n){
+    int a=0,b=1,c;
+    while(a<n)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    if(a==n)
+        return true;
+    else
+        return false;
+}
+
