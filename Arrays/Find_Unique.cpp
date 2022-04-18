03ea0b0a6447e8b6c4e9d896d919259210d58e44
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-04-18 20:53:27 +0530

    updated

---
+++ Arrays/Find_Unique.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "solution.h"
+using namespace std;
+
+int main()
+{
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int size;
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; ++i)
+		{
+			cin >> input[i];
+		}
+
+		cout << findUnique(input, size) << endl;
+	}
+
+	return 0;
+}
+
+int findUnique(int * arr, int size){
+    
+    for(int i=0; i<size;i++)
+    {
+        int j;
+        for(j=0;j<size;j++)
+        {
+            if(i!=j)
+            {
+                if(arr[i] == arr[j])
+                {
+                    break;
+               }
+            }
+        }
+        if(j==size){
+            return arr[i];
+        }
+    }
+}
+        
+    
+
