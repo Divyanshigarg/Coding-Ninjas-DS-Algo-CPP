3a0b6ee5dcad356d522f9c3063e1cbf3837b5af5
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-06-05 10:59:01 +0530

    find the duplicate in array

---
+++ Time and space complexity/duplicate_in_array.cpp
@@ -0,0 +1,57 @@
+Sample Input 1:
+1
+9
+0 7 2 5 4 7 1 3 6
+Sample Output 1:
+7
+Sample Input 2:
+2
+5
+0 2 1 3 1
+7
+0 3 1 5 4 3 2
+Sample Output 2:
+1
+3
+
+
+#include <iostream>
+using namespace std;
+
+#include "solution.h"
+
+int main()
+{
+	int t;
+	cin >> t;
+	
+	while (t--)
+	{
+		int size;
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			cin >> input[i];
+		}
+
+		cout << findDuplicate(input, size) << endl;
+	}
+
+	return 0;
+}
+
+
+int findDuplicate(int *arr, int n)
+{
+    int sum=0,res=0;
+    
+     res=(n-2)*(n-1)/2;
+    
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    return sum-res;
+}
