60cd3d84c2143e88163853e61f08c9588dcb9dd8
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-07-16 08:30:36 +0530

    updated

---
+++ Linked-List-2/delete_every_n_node.cpp
@@ -0,0 +1,133 @@
+Sample Input 1 :
+1
+1 2 3 4 5 6 7 8 -1
+2 2
+Sample Output 1 :
+1 2 5 6
+Sample Input 2 :
+2
+10 20 30 40 50 60 -1
+0 1
+1 2 3 4 5 6 7 8 -1
+2 3
+Sample Output 2 :
+1 2 6 7
+
+
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+    #include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int m, n;
+		cin >> m >> n;
+		head = skipMdeleteN(head, m, n);
+		print(head);
+	}
+	return 0;
+}
+
+Node *skipMdeleteN(Node *head, int M, int N)
+{
+	if(M==0)
+        return NULL;
+    
+    Node *temp=head;
+     Node *ptr;
+    int count=0;
+    
+    while(temp)
+    {
+        for( count=1;count<M and temp;count++)
+        {
+            temp=temp->next;
+        }
+      
+        if(temp==NULL)
+           return head;
+    
+   ptr=temp->next;
+   for( count=1;count<=N and ptr;count++)
+        {
+            ptr=ptr->next;
+        }
+    temp->next=ptr;
+    temp=ptr;
+}
+    return head;
+}
+
+
+
+
+
