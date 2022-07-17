68386354ffcf323c725e76d5949a4e0c59cdcc5a
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-07-17 08:51:04 +0530

    updated

---
+++ Linked-List-2/bubble_sort(iterative).cpp
@@ -0,0 +1,107 @@
+Sample Input 1 :
+10 9 8 7 6 5 4 3 -1
+Sample Output 1 :
+ 3 4 5 6 7 8 9 10 
+ Sample Input 2 :
+10 -5 9 90 5 67 1 89 -1
+Sample Output 2 :
+-5 1 5 9 10 67 89 90 
+
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+    //bubble sort iterative
+#include <iostream>
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+int main()
+{
+	Node *head = takeinput();
+	head = bubbleSort(head);
+	print(head);
+}
+Node *bubbleSort(Node *head)
+{
+	 if (head == NULL)
+        return head;
+    Node *current = head;
+    Node *tail = head;
+    while (tail->next != NULL)
+        tail = tail->next;
+    while (current != tail)
+    {
+        if (current->data > current->next->data)
+        {
+            int temp = current->next->data;
+            current->next->data = current->data;
+            current->data = temp;
+        }
+        if (current->next == tail)
+        {
+            tail = current;
+            current = head;
+        }
+        else
+            current = current->next;
+    }
+    return head;
+}
