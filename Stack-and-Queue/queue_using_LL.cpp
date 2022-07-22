ad547405a5340424dd9fa2b1951898dd92c80341
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-07-22 21:37:53 +0530

    updated

---
+++ Stack-and-Queue/queue_using_LL.cpp
@@ -0,0 +1,161 @@
+/************************************************************
+ 
+    Following is the structure of the node class 
+ 
+    class Node {
+    	public :
+    	int data;
+    	Node *next;
+
+    	Node(int data) {
+        	this->data = data;
+        	next = NULL;
+    	}
+	};
+
+**************************************************************/
+Sample Input 1:
+7
+1 17
+1 23
+1 11
+2
+2
+2
+2
+Sample Output 1:
+17
+23
+11
+-1
+Sample Input 2:
+3
+2
+1 10
+4
+Sample Output 2:
+-1 
+1
+#include <iostream>
+using namespace std;
+
+class Node {
+   public:
+    int data;
+    Node *next;
+
+    Node(int data) {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+#include "solution.h"
+
+int main() {
+    Queue q;
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int choice, input;
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cin >> input;
+                q.enqueue(input);
+                break;
+            case 2:
+                cout << q.dequeue() << "\n";
+                break;
+            case 3:
+                cout << q.front() << "\n";
+                break;
+            case 4:
+                cout << q.getSize() << "\n";
+                break;
+            default:
+                cout << ((q.isEmpty()) ? "true\n" : "false\n");
+                break;
+        }
+    }
+}
+
+
+class Queue
+{
+    // Define the data members
+    Node *head, *tail;
+    int size;
+
+public:
+    Queue()
+    {
+        // Implement the Constructor
+        head = NULL;
+        tail = NULL;
+        size = 0;
+    }
+
+    /*----------------- Public Functions of Stack -----------------*/
+
+    int getSize()
+    {
+        // Implement the getSize() function
+        return size;
+    }
+
+    bool isEmpty()
+    {
+        // Implement the isEmpty() function
+        return head == NULL;
+    }
+
+    void enqueue(int data)
+    {
+        // Implement the enqueue() function
+        Node *temp = new Node(data);
+        if (head == NULL)
+        {
+            head = temp;
+            tail = temp;
+        }
+        else
+        {
+            tail->next = temp;
+            tail = tail->next;
+        }
+        size++;
+    }
+
+    int dequeue()
+    {
+        // Implement the dequeue() function
+        if (isEmpty())
+            return -1;
+        Node *temp = NULL;
+        int num = head->data;
+        if (head == tail)
+        {
+            head = NULL;
+            tail = NULL;
+        }
+        else
+        {
+            temp = head->next;
+            delete head;
+            head = temp;
+        }
+        size--;
+        return num;
+    }
+
+    int front()
+    {
+        // Implement the front() function
+        if (isEmpty())
+            return -1;
+        return head->data;
+    }
+};
