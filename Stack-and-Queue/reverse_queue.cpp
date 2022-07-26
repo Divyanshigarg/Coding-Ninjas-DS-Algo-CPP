07d09f200584a02f2f32f0e4c5e8cb516b4fad41
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-07-26 07:03:45 +0530

    reverse the queue

---
+++ Stack-and-Queue/reverse_queue.cpp
@@ -0,0 +1,57 @@
+Sample Input 1:
+1
+6
+1 2 3 4 5 10
+Note:
+Here, 1 is at the front and 10 is at the rear of the queue.
+Sample Output 1:
+10 5 4 3 2 1
+Sample Input 2:
+2
+5
+2 8 15 1 10
+3
+10 20 30
+Sample Output 2:
+10 1 15 8 2 
+30 20 10 
+
+#include <iostream>
+using namespace std;
+
+#include "solution.h"
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        queue<int> q;
+        int size;
+        cin >> size;
+
+        for (int i = 0, val; i < size; i++) {
+            cin >> val;
+            q.push(val);
+        }
+
+        reverseQueue(q);
+        while (!q.empty()) {
+            cout << q.front() << " ";
+            q.pop();
+        }
+
+        cout << "\n";
+    }
+}
+
+#include<queue>
+void reverseQueue(queue<int> &input)
+{
+    if(input.empty())
+        return;
+    int x=input.front();
+    input.pop();
+    reverseQueue(input);
+    input.push(x);
+}
