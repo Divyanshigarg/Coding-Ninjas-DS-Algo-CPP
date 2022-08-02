b57f217318e09019bcd43e666df06ff5cb15b44c
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-08-02 20:16:56 +0530

    updated

---
+++ Tree/contain_X.cpp
@@ -0,0 +1,104 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+
+Sample Input 1 :
+10 3 20 30 40 2 40 50 0 0 0 0
+40 
+Sample Output 1 :
+true
+Sample Input 2 :
+10 3 20 30 40 2 40 50 0 0 0 0
+4 
+Sample Output 2:
+false
+    
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+#include "solution.h"
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    int x;
+    cin >> x;
+    cout << (isPresent(root, x) ? "true" : "false");
+}
+
+bool isPresent(TreeNode<int> *root, int x)
+{
+    // Write your code here
+    if (root == NULL)
+        return false;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        bool value = isPresent(root->children[i], x);
+        if (value == true)
+            return true;
+    }
+    if (root->data == x)
+        return true;
+    else
+        return false;
+}
