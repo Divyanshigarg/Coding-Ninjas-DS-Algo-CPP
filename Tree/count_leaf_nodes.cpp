727227d4fa66b96118f9ae674df766020211940d
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-08-03 09:29:17 +0530

    updated

---
+++ Tree/count_leaf_nodes.cpp
@@ -0,0 +1,98 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+
+Sample Input 1 :
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output 1 :
+4
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+#include "solution.h"
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    cout << getLeafNodeCount(root);
+}
+
+int getLeafNodeCount(TreeNode<int>* root) {
+    // Write your code here
+    if(root==NULL)
+        return 0;
+    if(root->children.size()==0)
+        return 1;
+    int count=0;
+    for(int i=0;i<root->children.size();i++)
+    {
+        count= count+getLeafNodeCount(root->children[i]);
+    }
+    return count;
+}
+
+
+    
+    
+
