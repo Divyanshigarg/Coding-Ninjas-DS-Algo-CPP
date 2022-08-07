69f608efa842d718460568dab2cd8ca4c0f64655
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-08-07 21:09:54 +0530

    updated

---
+++ Tree/max_data_nodes.cpp
@@ -0,0 +1,104 @@
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+Sample Input 1:
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output 1:
+50
+
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+#include "solution.h"
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    TreeNode<int>* ans = maxDataNode(root);
+
+    if (root != NULL) {
+        cout << ans->data;
+    }
+}
+
+TreeNode<int>* maxDataNode(TreeNode<int>* root) {
+    // Write your code here
+    if(root==NULL)
+        return NULL;
+    
+    int max=root->data;
+    
+    TreeNode<int>* Node=root;
+    
+    for(int i=0;i<root->children.size();i++)
+    {
+        TreeNode<int>* temp=maxDataNode(root->children[i]);
+        if(temp->data>max)
+        {
+            max=temp->data;
+            Node=temp;
+        }
+    }
+    return Node;
+}
